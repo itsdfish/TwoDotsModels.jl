d852d6efebabd0640a7a0d01e7932ed8b0c5e9d3
itsdfish <itsdfish@gmail.com>
2021-06-12 04:46:59 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/TwoDotsModels.jl
@@ -0,0 +1,5 @@
+module TwoDotsModels
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TwoDotsModels
+using Test
+
+@testset "TwoDotsModels.jl" begin
+    # Write your tests here.
+end
